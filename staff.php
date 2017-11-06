41f631d8cb0341492a265bdd09037471e47932c1
winoto j <minx1208@hotmail.com>
2017-11-05 20:35:15 -0500

    * changed var name

--- staff.php
+++ staff.php
@@ -1,4 +1,5 @@
 <?php
+
 if (false) {
     $app = new \Slim\Slim();
     $log = new Logger('main');
@@ -6,8 +7,8 @@ if (false) {
 
 
 
-$app->get('/signup', function() use($app){
-    
+$app->get('/signup', function() use($app) {
+
     $app->render('admin/create_doctor_account.html.twig');
 });
 
@@ -15,26 +16,23 @@ $app->post('/signup', function() use($app) {
     $firstName = $app->request()->post('first_name');
     $lastName = $app->request()->post('last_name');
     $email = $app->request()->post('email');
-    $street = $app->request()->post('street');
-    $city = $app->request()->post('city');
-    $province = $app->request()->post('province');
-    $postal = $app->request()->post('postal');
-    $country = $app->request()->post('country');
-    $sin = $app->request()->post('sin');
+    $title = $app->request()->post('title');
+    $speciality = $app->request()->post('speciality');
+    $experience = $app->request()->post('experience');
+    $description = $app->request()->post('description');
+    $pathImage = $app->request()->post('pathImage');
     $pass1 = $app->request()->post('user_password');
     $pass2 = $app->request()->post('confirm_password');
-    $phone = $app->request()->post('contact_no');
 //
     $values = array('first_name' => $firstName,
-                    'last_name' => $lastName,
-                    'email' => $email,
-                    'street' => $street,
-                    'city' => $city,
-                    'province' => $province,
-                    'postal' => $postal,
-                    'sin' => $sin,
-                    'contact_no' => $phone
-                    );
+        'last_name' => $lastName,
+        'email' => $email,
+        'title' => $title,
+        'speciality' => $speciality,
+        'experience' => $experience,
+        'description' => $description,
+        'pathImage' => $pathImage
+    );
     $errorList = array();
     $error = array();
 //
@@ -42,7 +40,6 @@ $app->post('/signup', function() use($app) {
         $values['first_name'] = '';
         //array_push($errorList, "First Name must be between 2 and 50 characters long");
         $error['fNameError'] = 'First name must be between 2 and 50 characters long';
-        
     }
     if (strlen($lastName) < 2 || strlen($lastName) > 50) {
         $values['last_name'] = '';
@@ -61,49 +58,67 @@ $app->post('/signup', function() use($app) {
             $error['emailError'] = 'Email already in use';
         }
     }
-    if (strlen($street) < 4 || strlen($street) > 50) {
-        $values['street'] = '';
+    if (strlen($title) < 2 || strlen($title) > 25) {
+        $values['title'] = '';
         //array_push($errorList, "Address must be between 4 and 50 characters long");
-        $error['addressError'] = "Address must be between 4 and 50 characters long";
+        $error['titleError'] = "Title must be between 2 and 25 characters long";
     }
-    if (strlen($city) < 2 || strlen($city) > 50) {
+    if (strlen($speciality) < 2 || strlen($speciality) > 50) {
         $values['city'] = '';
         //array_push($errorList, "City must be between 2 and 50 characters long");
-        $error['cityError'] = "City must be between 4 and 50 characters long";
+        $error['cityError'] = "Speciality must be between 2 and 50 characters long";
     }
-    if ($province != 'AB' || $province != 'BC' || $province != 'MB' || $province != 'NB' ||
-            $province != 'NL' || $province != 'NS' || $province != 'NU' || $province != 'ON' ||
-            $province != 'PE'  || $province != 'QC' || $province != 'SK' || $province != 'YT') {
+    if (!checkdate(1, 1, $experience)) {
+        $values['experience'] = '';
         //array_push($errorList, "City must be between 2 and 50 characters long");
-        $error['provinceError'] = "Choose province from the list";
+        $error['experience'] = "Enter graduated year";
     }
-    if (strlen($postal) < 6 || strlen($postal) > 7) {
-        $values['postal'] = '';
-        //array_push($errorList, "City must be between 2 and 50 characters long");
-        $error['postalError'] = "Postal code format H0H 0H0";
-    }
-    
+
     if ($pass1 != $pass2) {
         //array_push($errorList, "Passwords don't match");
         $error['pass1Error'] = "Password do not match";
         $error['pass2Error'] = "Password do not match";
     } else { // TODO: do a better check for password quality (lower/upper/numbers/special)
-        
         if (!preg_match('/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9_!#@\$\^&\(\)\{\}\[\],\.<>;:\'\"`~%\*\-\+]{6,100}/', $pass1)) {
             //array_push($errorList, "Password must include at least one character in each three categories: "
             //        . "uppercase letter, lowercase letter, digit or special character");
             $error['pass1Error'] = "Minimum 6 chars and at least 1 Capital, 1 lower 1 number and special Char";
         }
     }
-    if (strlen($sin) < 9 || strlen($sin) > 11) {
+    if (strlen($description) < 10 || strlen($sin) > 1000) {
         $values['sin'] = '';
         //array_push($errorList, "City must be between 2 and 50 characters long");
-        $error['sinError'] = "SIN must be 9 numeric character";
+        $error['descriptionError'] = "Description must be between 10 to 1000 chars";
     }
-    if (strlen($postal) < 6 || strlen($postal) > 7) {
-        $values['postal'] = '';
-        //array_push($errorList, "City must be between 2 and 50 characters long");
-        $error['phoneError'] = "Phone (514) 123-4567";
+    $doctorImage = array();
+
+//doesnt work if only with isset	
+    if (isset($_FILES['doctorImage']) && !empty($_FILES['doctorImage']['name'])) {
+        $doctorImage = $_FILES['doctorImage'];
+        if ($doctorImage['error'] != 0) {
+            array_push($errorList, "Error uploading file");
+            $log->err("Error uploading file: " . print_r($doctorImage, true));
+        } else {
+            if (strstr($doctorImage['name'], '..')) {
+                array_push($errorList, "Invalid file name");
+                $log->warn("Uploaded file name with .. in it (possible attack): " . print_r($doctorImage, true));
+            }
+// TODO: check if file already exists, check maximum size of the file, dimensions of the image etc.
+            $info = getimagesize($doctorImage["tmp_name"]);
+            if ($info == FALSE) {
+                array_push($errorList, "File doesn't look like a valid image");
+            } else {
+                if ($info['mime'] == 'image/jpeg' || $info['mime'] == 'image/gif' || $info['mime'] == 'image/png') {
+// image type is valid - all good
+                } else {
+                    array_push($errorList, "Image must be a JPG, GIF, or PNG only.");
+                }
+            }
+        }
+    } else { // no file uploaded
+        if ($op == 'add') {
+            array_push($errorList, "Image is required when creating new doctor");
+        }
     }
 
     if ($errorList) { // 3. failed submission
@@ -111,11 +126,12 @@ $app->post('/signup', function() use($app) {
             'errorList' => $errorList,
             'v' => $values,
             'e' => $error));
-    } else { // 2. successful submission
+    } else {
+        // 2. successful submission
         $passEnc = password_hash($pass1, PASSWORD_BCRYPT);
-        DB::insert('patients', array('firstName' => $firstName, 'lastName' => $lastName,
-            'street' => $street, 'city' => $city, 'province' => $province, 'postal' => $postal,
-            'country' => $country, 'sin' => $sin, 'email' => $email, 'password' => $passEnc, 'phone' => $phone));
+        DB::insert('doctors', array('firstName' => $firstName, 'lastName' => $lastName,
+            'title' => $title, 'speciality' => $speciality, 'experience' => $experience, 'description' => $description,
+            'pathImage' => $country, 'sin' => $sin, 'email' => $email, 'password' => $passEnc, 'phone' => $phone));
         $app->render('register_success.html.twig');
     }
 });
@@ -127,29 +143,29 @@ $app->get('/isemailregistered/:email', function($email) use ($app) {
 });
 
 //---------------------------------------------------GET SIGNIN----------------------
-$app->get('/sigin', function() use ($app){
+$app->get('/sigin', function() use ($app) {
     $app->render('admin/signin.html.twig');
 });
 
 //--------------------------------------------------POST LOGIN---------------------
-$app->post('/sigin', function() use($app){
+$app->post('/sigin', function() use($app) {
     $email = $app->request()->post('email');
-$pass = $app->request()->post('password');
-$row = DB::queryFirstRow("SELECT * FROM patients WHERE email=%s", $email);
-$error = true;
-if ($row) {
-if(password_verify($pass, $row['password'])){
+    $pass = $app->request()->post('password');
+    $row = DB::queryFirstRow("SELECT * FROM patients WHERE email=%s", $email);
+    $error = true;
+    if ($row) {
+        if (password_verify($pass, $row['password'])) {
 //if ($pass == $row['password']) {
-$error = false;
-}
-}
-if ($error) {
-$app->render('admin/sigin.html.twig', array('error' => true));
-} else {
-unset($row['password']);
-$_SESSION['staff'] = $row;
+            $error = false;
+        }
+    }
+    if ($error) {
+        $app->render('admin/sigin.html.twig', array('error' => true));
+    } else {
+        unset($row['password']);
+        $_SESSION['staff'] = $row;
 //$app->render('login_success.html.twig', array('userSession' => $_SESSION['user']));
-}
+    }
 });
 
 $app->run();
