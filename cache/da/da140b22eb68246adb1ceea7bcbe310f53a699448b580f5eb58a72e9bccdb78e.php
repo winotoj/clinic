78b01320e7af53bf876fea0939f519205213fe6b
winoto j <minx1208@hotmail.com>
2017-11-08 17:22:34 -0500

    * need to do the ui

--- cache/da/da140b22eb68246adb1ceea7bcbe310f53a699448b580f5eb58a72e9bccdb78e.php
+++ cache/da/da140b22eb68246adb1ceea7bcbe310f53a699448b580f5eb58a72e9bccdb78e.php
@@ -26,57 +26,40 @@ class __TwigTemplate_746f624b89248ab1d158528736c44844a69ae876f8fb191ca13edcdf3e2
         // line 4
         echo twig_escape_filter($this->env, $this->getAttribute((isset($context["firstday"]) ? $context["firstday"] : null), 0, array(), "array"), "html", null, true);
         echo "</p>
-       <table>
-         ";
-        // line 6
-        $context["dat"] = 1;
-        // line 7
-        echo "        ";
-        $context['_parent'] = $context;
-        $context['_seq'] = twig_ensure_traversable(range(1, (isset($context["datelist"]) ? $context["datelist"] : null)));
-        foreach ($context['_seq'] as $context["_key"] => $context["day"]) {
-            // line 8
-            echo "             <tr>
-                 ";
-            // line 9
-            $context["foo"] = 1;
-            // line 10
-            echo "                 
-            ";
-            // line 11
-            $context['_parent'] = $context;
-            $context['_seq'] = twig_ensure_traversable(range(0, ((isset($context["datelist"]) ? $context["datelist"] : null) - 1)));
-            foreach ($context['_seq'] as $context["_key"] => $context["num"]) {
-                if (((isset($context["foo"]) ? $context["foo"] : null) < 8)) {
-                    // line 12
-                    echo "                <td>";
-                    echo twig_escape_filter($this->env, $this->getAttribute((isset($context["firstday"]) ? $context["firstday"] : null), $context["num"], array(), "array"), "html", null, true);
-                    echo " ";
-                    echo twig_escape_filter($this->env, (isset($context["dat"]) ? $context["dat"] : null), "html", null, true);
-                    echo " </td> 
-                ";
-                    // line 13
-                    $context["foo"] = ((isset($context["foo"]) ? $context["foo"] : null) + 1);
-                    // line 14
-                    echo "                ";
-                    $context["dat"] = ((isset($context["dat"]) ? $context["dat"] : null) + 1);
-                    // line 15
-                    echo "            ";
-                }
-            }
-            $_parent = $context['_parent'];
-            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['num'], $context['_parent'], $context['loop']);
-            $context = array_intersect_key($context, $_parent) + $_parent;
-            // line 16
-            echo "            </tr>
-            
+       <form>
         ";
+        // line 6
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(range(0, ((isset($context["datelist"]) ? $context["datelist"] : null) - 1)));
+        foreach ($context['_seq'] as $context["_key"] => $context["day"]) {
+            // line 7
+            echo "            ";
+            if ((((($context["day"] == 7) || ($context["day"] == 14)) || ($context["day"] == 21)) || ($context["day"] == 28))) {
+                // line 8
+                echo "                <br>
+            ";
+            }
+            // line 10
+            echo "            <div class=\"drschedule\">
+            <label> ";
+            // line 11
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["firstday"]) ? $context["firstday"] : null), $context["day"], array(), "array"), "html", null, true);
+            echo " ";
+            echo twig_escape_filter($this->env, ($context["day"] + 1), "html", null, true);
+            echo " </label><br>
+            <input type=\"checkbox\" name=\"doctorwork";
+            // line 12
+            echo twig_escape_filter($this->env, ($context["day"] + 1), "html", null, true);
+            echo "\" value=\"true\">
+            </div>
+            ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['day'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 19
-        echo "       </table>
+        // line 15
+        echo "
+       </form>
     </div>
     ";
     }
@@ -93,7 +76,7 @@ class __TwigTemplate_746f624b89248ab1d158528736c44844a69ae876f8fb191ca13edcdf3e2
 
     public function getDebugInfo()
     {
-        return array (  79 => 19,  71 => 16,  64 => 15,  61 => 14,  59 => 13,  52 => 12,  47 => 11,  44 => 10,  42 => 9,  39 => 8,  34 => 7,  32 => 6,  27 => 4,  23 => 3,  19 => 1,);
+        return array (  61 => 15,  52 => 12,  46 => 11,  43 => 10,  39 => 8,  36 => 7,  32 => 6,  27 => 4,  23 => 3,  19 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -110,21 +93,18 @@ class __TwigTemplate_746f624b89248ab1d158528736c44844a69ae876f8fb191ca13edcdf3e2
     <div id=\"viewSchedule\">
         <p>{{ datelist }}</p>
        <p>{{ firstday[0] }}</p>
-       <table>
-         {% set dat = 1 %}
-        {% for day in range(1, datelist ) %}
-             <tr>
-                 {% set foo = 1 %}
-                 
-            {% for num in range(0,datelist-1) if foo < 8  %}
-                <td>{{ firstday[num] }} {{ dat }} </td> 
-                {% set foo = foo + 1 %}
-                {% set dat = dat + 1 %}
+       <form>
+        {% for day in range(0, datelist-1 ) %}
+            {% if day == 7 or day == 14 or day == 21 or day ==28 %}
+                <br>
+            {% endif %}
+            <div class=\"drschedule\">
+            <label> {{ firstday[day] }} {{ day+1 }} </label><br>
+            <input type=\"checkbox\" name=\"doctorwork{{ day+1 }}\" value=\"true\">
+            </div>
             {% endfor %}
-            </tr>
-            
-        {% endfor %}
-       </table>
+
+       </form>
     </div>
     ", "admin/ajaxdoctor_schedule.html.twig", "C:\\xampp\\htdocs\\php-project\\templates\\admin\\ajaxdoctor_schedule.html.twig");
     }
