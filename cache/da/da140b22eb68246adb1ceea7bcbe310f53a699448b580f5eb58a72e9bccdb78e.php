e08e39e93cfccce7b7f8b3672642ceaaab161ad9
winoto j <minx1208@hotmail.com>
2017-11-08 19:17:31 -0500

    * ajax schedule need id for each field, save button

--- cache/da/da140b22eb68246adb1ceea7bcbe310f53a699448b580f5eb58a72e9bccdb78e.php
+++ cache/da/da140b22eb68246adb1ceea7bcbe310f53a699448b580f5eb58a72e9bccdb78e.php
@@ -17,16 +17,16 @@ class __TwigTemplate_746f624b89248ab1d158528736c44844a69ae876f8fb191ca13edcdf3e2
     {
         // line 1
         echo "
-    <div id=\"viewSchedule\">
-        <p>";
+<div id=\"viewSchedule\">
+    <p>";
         // line 3
         echo twig_escape_filter($this->env, (isset($context["datelist"]) ? $context["datelist"] : null), "html", null, true);
         echo "</p>
-       <p>";
+    <p>";
         // line 4
         echo twig_escape_filter($this->env, $this->getAttribute((isset($context["firstday"]) ? $context["firstday"] : null), 0, array(), "array"), "html", null, true);
         echo "</p>
-       <form>
+    <form class=\"form-horizontal\">
         ";
         // line 6
         $context['_parent'] = $context;
@@ -34,34 +34,76 @@ class __TwigTemplate_746f624b89248ab1d158528736c44844a69ae876f8fb191ca13edcdf3e2
         foreach ($context['_seq'] as $context["_key"] => $context["day"]) {
             // line 7
             echo "            ";
-            if ((((($context["day"] == 7) || ($context["day"] == 14)) || ($context["day"] == 21)) || ($context["day"] == 28))) {
+            if (((((($context["day"] == 0) || ($context["day"] == 7)) || ($context["day"] == 14)) || ($context["day"] == 21)) || ($context["day"] == 28))) {
                 // line 8
-                echo "                <br>
-            ";
+                echo "                <div class=\"row\">
+                ";
             }
             // line 10
-            echo "            <div class=\"drschedule\">
-            <label> ";
-            // line 11
+            echo "
+                <div class=\"col-sm-1\">
+                    <div class=\"input-group\">
+                        <span class=\"input-group-addon\"></span>
+                        <label class=\"form-control form-horizontal\">";
+            // line 14
             echo twig_escape_filter($this->env, $this->getAttribute((isset($context["firstday"]) ? $context["firstday"] : null), $context["day"], array(), "array"), "html", null, true);
-            echo " ";
+            echo "&nbsp;";
             echo twig_escape_filter($this->env, ($context["day"] + 1), "html", null, true);
-            echo " </label><br>
-            <input type=\"checkbox\" name=\"doctorwork";
-            // line 12
+            echo "</label>
+                        <label class=\"form-check-label form-horizontal\">Available
+                            <input class=\"form-control form-check-input\" type=\"checkbox\" name=\"doctorwork";
+            // line 16
             echo twig_escape_filter($this->env, ($context["day"] + 1), "html", null, true);
-            echo "\" value=\"true\">
-            </div>
+            echo "\" value=\"true\" checked></label>
+                        <div class=\"inform-inline\">
+                             <label for=\"startTime\" class=\"form-horizontal\">Start
+                            <select class=\"form-control\" class=\"startTime\">
+                                <option>8:00</option>
+                                <option>9:00</option>
+                                <option>10:00</option>
+                                <option>11:00</option>
+                                <option>12:00</option>
+                                <option>13:00</option>
+                                <option>14:00</option>
+                                <option>15:00</option>
+                                <option>16:00</option>
+                                <option>17:00</option>
+                            </select></label>
+                            <label for=\"endTime\" class=\"form-horizontal\">End
+                            <select class=\"form-control\" class=\"endTime\">
+                                <option>8:00</option>
+                                <option>9:00</option>
+                                <option>10:00</option>
+                                <option>11:00</option>
+                                <option>12:00</option>
+                                <option>13:00</option>
+                                <option>14:00</option>
+                                <option>15:00</option>
+                                <option>16:00</option>
+                                <option>17:00</option>
+                            </select></label>     
+                        </div>
+                    </div>
+                </div>
+                ";
+            // line 47
+            if (((((($context["day"] == 6) || ($context["day"] == 13)) || ($context["day"] == 20)) || ($context["day"] == 27)) || ($context["day"] == ((isset($context["datelist"]) ? $context["datelist"] : null) - 1)))) {
+                // line 48
+                echo "                </div>
             ";
+            }
+            // line 50
+            echo "        ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['day'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 15
+        // line 51
         echo "
-       </form>
-    </div>
-    ";
+    </form>
+</div>
+
+";
     }
 
     public function getTemplateName()
@@ -76,7 +118,7 @@ class __TwigTemplate_746f624b89248ab1d158528736c44844a69ae876f8fb191ca13edcdf3e2
 
     public function getDebugInfo()
     {
-        return array (  61 => 15,  52 => 12,  46 => 11,  43 => 10,  39 => 8,  36 => 7,  32 => 6,  27 => 4,  23 => 3,  19 => 1,);
+        return array (  102 => 51,  96 => 50,  92 => 48,  90 => 47,  56 => 16,  49 => 14,  43 => 10,  39 => 8,  36 => 7,  32 => 6,  27 => 4,  23 => 3,  19 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -90,22 +132,59 @@ class __TwigTemplate_746f624b89248ab1d158528736c44844a69ae876f8fb191ca13edcdf3e2
     public function getSourceContext()
     {
         return new Twig_Source("
-    <div id=\"viewSchedule\">
-        <p>{{ datelist }}</p>
-       <p>{{ firstday[0] }}</p>
-       <form>
+<div id=\"viewSchedule\">
+    <p>{{ datelist }}</p>
+    <p>{{ firstday[0] }}</p>
+    <form class=\"form-horizontal\">
         {% for day in range(0, datelist-1 ) %}
-            {% if day == 7 or day == 14 or day == 21 or day ==28 %}
-                <br>
-            {% endif %}
-            <div class=\"drschedule\">
-            <label> {{ firstday[day] }} {{ day+1 }} </label><br>
-            <input type=\"checkbox\" name=\"doctorwork{{ day+1 }}\" value=\"true\">
-            </div>
-            {% endfor %}
+            {% if day ==0 or day == 7 or day == 14 or day == 21 or day ==28 %}
+                <div class=\"row\">
+                {% endif %}
 
-       </form>
-    </div>
-    ", "admin/ajaxdoctor_schedule.html.twig", "C:\\xampp\\htdocs\\php-project\\templates\\admin\\ajaxdoctor_schedule.html.twig");
+                <div class=\"col-sm-1\">
+                    <div class=\"input-group\">
+                        <span class=\"input-group-addon\"></span>
+                        <label class=\"form-control form-horizontal\">{{ firstday[day] }}&nbsp;{{ day+1 }}</label>
+                        <label class=\"form-check-label form-horizontal\">Available
+                            <input class=\"form-control form-check-input\" type=\"checkbox\" name=\"doctorwork{{ day+1 }}\" value=\"true\" checked></label>
+                        <div class=\"inform-inline\">
+                             <label for=\"startTime\" class=\"form-horizontal\">Start
+                            <select class=\"form-control\" class=\"startTime\">
+                                <option>8:00</option>
+                                <option>9:00</option>
+                                <option>10:00</option>
+                                <option>11:00</option>
+                                <option>12:00</option>
+                                <option>13:00</option>
+                                <option>14:00</option>
+                                <option>15:00</option>
+                                <option>16:00</option>
+                                <option>17:00</option>
+                            </select></label>
+                            <label for=\"endTime\" class=\"form-horizontal\">End
+                            <select class=\"form-control\" class=\"endTime\">
+                                <option>8:00</option>
+                                <option>9:00</option>
+                                <option>10:00</option>
+                                <option>11:00</option>
+                                <option>12:00</option>
+                                <option>13:00</option>
+                                <option>14:00</option>
+                                <option>15:00</option>
+                                <option>16:00</option>
+                                <option>17:00</option>
+                            </select></label>     
+                        </div>
+                    </div>
+                </div>
+                {% if day ==6 or day == 13 or day == 20 or day == 27 or day == datelist-1 %}
+                </div>
+            {% endif %}
+        {% endfor %}
+
+    </form>
+</div>
+
+", "admin/ajaxdoctor_schedule.html.twig", "C:\\xampp\\htdocs\\php-project\\templates\\admin\\ajaxdoctor_schedule.html.twig");
     }
 }
