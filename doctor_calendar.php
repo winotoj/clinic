40c44bf42e73518f53e7eac371b4beead2b46404
winoto j <minx1208@hotmail.com>
2017-11-12 13:28:49 -0500

    * doctor calendar is fixed (could add note and see patient history}

--- doctor_calendar.php
+++ doctor_calendar.php
@@ -18,8 +18,17 @@ $app->get('/events', function() use ($app, $log) {
     $json = array();
 
     // Query that retrieves events
-
-    $cal = DB::query('SELECT a.id as id, d.id as doctorId, d.firstName as doctorFirstName, d.lastName as doctorLastName, p.id as patientId, p.firstName as firstName, p.lastName as lastName, a.date, a.startTime, a.endTime, a.patientMessage as msg, a.doctorNote as note, a.readStatus as status, a.type as type FROM doctors as d JOIN appointments as a ON d.id = a.doctorId JOIN patients AS p ON a.patientId = p.id WHERE d.id = 1');
+//    $cal = DB::query('SELECT a.id as id, d.id as doctorId, d.firstName as doctorFirstName, '
+//                    . 'd.lastName as doctorLastName, p.id as patientId, p.firstName as firstName, '
+//                    . 'p.lastName as lastName, a.date, a.startTime, a.endTime, a.patientMessage as msg, '
+//                    . 'a.doctorNote as note, a.readStatus as status, a.type as type FROM doctors as d '
+//                    . 'JOIN appointments as a ON d.id = a.doctorId JOIN patients AS p ON a.patientId = p.id '
+//                    . 'WHERE d.id = 1');
+    $cal = DB::query('SELECT a.id as id, d.id as doctorId, d.firstName as doctorFirstName, '
+                    . 'd.lastName as doctorLastName, p.id as patientId, p.firstName as firstName, '
+                    . 'p.lastName as lastName, a.date, a.startTime, a.endTime, a.patientMessage as msg, '
+                    . 'a.doctorNote as note, a.readStatus as status, a.type as type FROM doctors as d '
+                    . 'JOIN appointments as a ON d.id = a.doctorId JOIN patients AS p ON a.patientId = p.id ');
     if (!$cal) {
         http_response_code(500);
         $app->render('internal_error.html.twig');
@@ -70,14 +79,20 @@ $app->post('/add_note_event', function() use ($app, $log) {
 });
 
 $app->post('/load_history', function() use ($app, $log) {
-    $patientId = $_POST('patientId');
-    $history = DB::query('SELECT d.firstName as dfirstName, d.lastName as dLastName, '
-                    . ' a.date as date, a.patientMessage as message, a.doctorNote as note, '
-                    . ' p.firstName as pfirstName, p.lastName as plastName FROM doctors AS d'
-                    . ' JOIN appointments as a ON d.id = a.doctorId JOIN patients as p on a.patientId = p.id'
-                    . ' WHERE a.patientId = %i order by a.date ASC', $patientId);
-    if (!$history) { //TODO FIX ERROR
-        return;
+    $patientId = $_POST['patientId'];
+    $history = DB::query('SELECT d.firstName as dfirstName, d.lastName as dlastName, a.date as date, a.patientMessage as message, a.doctorNote as note, p.firstName as pfirstName, p.lastName as plastName FROM doctors AS d JOIN appointments as a ON d.id = a.doctorId JOIN patients as p on a.patientId = p.id WHERE a.patientId = %i and a.date < CURRENT_DATE() order by a.date ASC', $patientId);
+
+    if (!$history) {
+        if(empty($history)){
+       $patient = DB::queryFirstRow('SELECT * FROM patients WHERE id = %i', $patientId);
+        $app->render('admin/patient_history.html.twig', array('p' => $patient));
+        }else{
+             http_response_code(500);
+        $app->render('internal_error.html.twig');
+        }
+    }else{
+        $app->render('admin/patient_history.html.twig', array('hh' => $history));
     }
-    $app->render('patient_history.html.twig', array('history' => $$history));
+   
+    
 });
